f9587a7ab896d3f008b8395d741fe198396091ae
jpriolo <centos@ip-10-0-1-249.us-west-2.compute.internal>
2017-07-16 01:06:57 +0000

    cookbooks directory - reorganized

---
+++ workstation/recipes/setup.rb
@@ -0,0 +1,25 @@
+package 'nano'
+package 'emacs'
+
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+  content 'This server is the property of ...
+hi there from Joseph P.'
+  action :create
+  owner 'root'
+  group 'root'
+  mode  '777'
+end
+
+service 'ntpd'  do
+  action  [:enable, :start]
+end

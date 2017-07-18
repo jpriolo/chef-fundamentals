90bf9f203ffd251a7406db2b2ef25937a6da6a16
jpriolo <joepp379@gmail.com>
2017-07-18 00:28:51 +0000

    finishing for the day 7.17.17

--- cookbooks/apache/recipes/server-jp.rb
+++ cookbooks/apache/recipes/server-jp.rb
@@ -1,5 +1,9 @@
 # ~/cookbooks/apache/recipes/server.rb
 
+package 'vim-enhanced' do
+  action :install
+end
+
 package 'httpd' do
   action  :install
 end
@@ -13,3 +17,20 @@ service 'httpd' do
   action [ :enable, :start ]
 end
 
+directory '/var/www/html/testfolder' do
+	owner			'apache'
+	group			'apache'
+	recursive		true
+	mode			'0755'
+end
+
+user 'user1' do
+	comment 'user1'
+	uid '123'
+	home '/home/user1'
+	shell '/bin/bash'
+end
+
+group 'admins222' do
+	members 'user1'
+end

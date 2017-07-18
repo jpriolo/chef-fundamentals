9341592fdd94424fb90dcba63d998ee033a9de74
jpriolo <joepp379@gmail.com>
2017-07-18 03:05:08 +0000

    subscribed service httpd to restart when index.html is modified

--- cookbooks/apache/recipes/server-jp.rb
+++ cookbooks/apache/recipes/server-jp.rb
@@ -8,13 +8,21 @@ package 'httpd' do
   action  :install
 end
 
+#---------
+#notifies :action, 'resource[name]', :timer
+#subscribes :action, 'resouce[name]', :timer
+#---------
+
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
-  action  :create
+  notifies  :restart, 'service[httpd]', :immediately
 end
 
+#subscribes :action, 'resouce[name]', :timer
 service 'httpd' do
   action [ :enable, :start ]
+  subscribes  :restart, 'template[/var/www/html/index.html]', :immediately
 end
 
 directory '/var/www/html/testfolder' do
@@ -30,7 +38,7 @@ user 'user1' do
 	home '/home/user1'
 	shell '/bin/bash'
 end
-
+#add group
 group 'admins222' do
 	members 'user1'
 end
